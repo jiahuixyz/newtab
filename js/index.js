2c7540ecb5fb7451dbd783c62d31f49d35943c52
liujiahui <936775057@qq.com>
2025-06-20 15:34:22 +0800

    优化

--- js/index.js
+++ js/index.js
@@ -1,7 +1,5 @@
 // 初始化搜索按钮选中状态
 initBtnSelected();
-// 初始化格言
-initMotto();
 
 // 初始化搜索按钮选中状态
 function initBtnSelected() {
@@ -17,46 +15,6 @@ function initBtnSelected() {
                 }
             }
         });
-    }
-}
-
-// 初始化格言
-function initMotto() {
-    const nowDate = getNowDate();
-    let item = localStorage.getItem(nowDate);
-    if (item) {
-        let obj = JSON.parse(item);
-        if (obj.hasOwnProperty('mottoIndex')) {
-            setMottoByIndex(obj.mottoIndex);
-            return;
-        }
-    }
-
-    // 对mottoArray的length取模,保证同一天的格言都一样，且mottoArray里的每张图片都能被用到
-    let mottoIndex = getDayOfYear() % mottoArray.length;
-
-    setMottoByIndex(mottoIndex);
-}
-
-function setMottoByIndex(mottoIndex) {
-    let mottoDiv = document.querySelector("#motto");
-    mottoDiv.textContent = "「 " + mottoArray[mottoIndex].content + " 」";
-}
-
-function changeMotto() {
-    const nowDate = getNowDate();
-    let randomInt = getRandomInt(0, mottoArray.length);
-
-    setMottoByIndex(randomInt);
-
-    let item = localStorage.getItem(nowDate);
-    if (item) {
-        let obj = JSON.parse(item);
-        obj.mottoIndex = randomInt;
-        localStorage.setItem(nowDate, JSON.stringify(obj));
-    } else {
-        let obj = {mottoIndex: randomInt};
-        localStorage.setItem(nowDate, JSON.stringify(obj));
     }
 }
 
---
+++ js/motto.js
@@ -0,0 +1,42 @@
+// 初始化格言
+function initMotto() {
+    const nowDate = getNowDate();
+    let item = localStorage.getItem(nowDate);
+    if (item) {
+        let obj = JSON.parse(item);
+        if (obj.hasOwnProperty('mottoIndex')) {
+            setMottoByIndex(obj.mottoIndex);
+            return;
+        }
+    }
+
+    // 对mottoArray的length取模,保证同一天的格言都一样，且mottoArray里的每张图片都能被用到
+    let mottoIndex = getDayOfYear() % mottoArray.length;
+
+    setMottoByIndex(mottoIndex);
+}
+
+function setMottoByIndex(mottoIndex) {
+    let mottoDiv = document.querySelector("#motto");
+    mottoDiv.textContent = "「 " + mottoArray[mottoIndex].content + " 」";
+}
+
+function changeMotto() {
+    const nowDate = getNowDate();
+    let randomInt = getRandomInt(0, mottoArray.length);
+
+    setMottoByIndex(randomInt);
+
+    let item = localStorage.getItem(nowDate);
+    if (item) {
+        let obj = JSON.parse(item);
+        obj.mottoIndex = randomInt;
+        localStorage.setItem(nowDate, JSON.stringify(obj));
+    } else {
+        let obj = {mottoIndex: randomInt};
+        localStorage.setItem(nowDate, JSON.stringify(obj));
+    }
+}
+
+// 初始化格言
+initMotto();
