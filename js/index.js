b41493f9b6f7ebe9f680e42195012d25454f9683
liujiahui <936775057@qq.com>
2025-06-22 19:55:16 +0800

    搜索建议demo

--- js/index.js
+++ js/index.js
@@ -1,19 +1,81 @@
 // 初始化搜索按钮选中状态
 initBtnSelected();
 
-// const inputElement = document.getElementById('search');
-//
-// inputElement.addEventListener('input', function(event) {
-//     console.log('输入的值:', event.target.value);
-//     const searchSug = document.getElementById('search-sug');
-//     searchSug.style.display = 'block';
-// });
+//初始化搜索建议
+// initSearchSug();
+
+function initSearchSug() {
+    const inputElement = document.getElementById('search');
+    const searchSug = document.getElementById('search-sug');
+
+    let isComposing = false; // 标记是否在输入法输入中
+
+    inputElement.addEventListener('compositionstart', () => {
+        isComposing = true;
+        console.log('isComposing = true;')
+    });
+
+    inputElement.addEventListener('compositionend', (event) => {
+        isComposing = false;
+        console.log('isComposing = false;')
+        if (event.target.value) {
+            updateSearchSug(event.target.value);
+        }
+    });
+
+    inputElement.addEventListener('input', function (event) {
+        if (isComposing) {
+            return;
+        }
+        if (event.target.value) {
+            updateSearchSug(event.target.value);
+        }
+        searchSug.innerHTML = '';
+    });
 
 // inputElement.addEventListener('blur', function(event) {
 //     console.log('输入框失去焦点，当前值:', event.target.value);
 //     const searchSug = document.getElementById('search-sug');
 //     searchSug.style.display = 'none';
 // });
+}
+
+function updateSearchSug(word) {
+    const searchSug = document.getElementById('search-sug');
+    searchSug.style.display = 'block';
+
+    //组装查询地址
+    var sugurl = `https://suggestion.baidu.com/su?wd=${word}&cb=window.baidu.sug`;
+
+    //定义回调函数
+    window.baidu = {
+        sug: function (json) {
+            console.log(json)
+            let suggestions = json.s;
+            let length = Math.min(suggestions.length, 4);
+            for (let i = 0; i < length; i++) {
+                // 创建新div元素
+                const childDiv = document.createElement('div');
+
+                // 设置子div内容
+                childDiv.textContent = suggestions[i];
+
+                // 添加子元素按钮事件
+                childDiv.addEventListener('click', function () {
+                    window.open(`https://www.baidu.com/baidu?wd=${suggestions[i]}`);
+                });
+
+                // 添加到父容器
+                searchSug.appendChild(childDiv);
+            }
+        }
+    }
+
+    //动态添加JS脚本
+    var script = document.createElement("script");
+    script.src = sugurl;
+    document.getElementsByTagName("head")[0].appendChild(script);
+}
 
 // 初始化搜索按钮选中状态
 function initBtnSelected() {
