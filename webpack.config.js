826da964a2d8ce212f32e0610986768d50bc617a
liujiahui <936775057@qq.com>
2025-07-22 01:06:23 +0800

    add workbox

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const CopyPlugin = require("copy-webpack-plugin");
+const WorkboxPlugin = require('workbox-webpack-plugin');
 
 const resolve = (pathStr) => {
 	return path.resolve(__dirname, pathStr);
@@ -33,6 +34,23 @@ module.exports = {
 					to: resolve("dist/img"),
 				}
 			]
-		})
+		}),
+		new WorkboxPlugin.GenerateSW({
+			clientsClaim: true,
+			skipWaiting: true,
+			runtimeCaching: [
+				{
+					urlPattern: /.(?:js|css|html|ico|jpg)$/,
+					handler: 'CacheFirst',
+					options: {
+						cacheName: 'static-resources',
+						expiration: {
+							maxEntries: 100,
+							maxAgeSeconds: 24 * 60 * 60,
+						},
+					},
+				}
+			],
+		}),
 	]
 };
