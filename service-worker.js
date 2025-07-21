761f1f2cc8d37a497961c0cf59a8aeaaea24037d
jiahuixyz <936775057@qq.com>
2025-07-21 17:25:10 +0000

    Deploying to gh-pages from @ jiahuixyz/newtab@224234c6005c44f68432dd7ca7d661bec71a8105 🚀

--- service-worker.js
+++ service-worker.js
@@ -88,7 +88,7 @@ define(['./workbox-9dc299d5'], (function (workbox) { 'use strict';
     "revision": "a835f887a3a5106983bcb333ab1b6401"
   }, {
     "url": "index.js",
-    "revision": "0c5f9689c31063104eaaf817224998c2"
+    "revision": "f61cc728914eb0a3be8463e12a923511"
   }], {});
   workbox.registerRoute(/.(?:js|css|html|ico|jpg)$/, new workbox.CacheFirst({
     "cacheName": "static-resources",
