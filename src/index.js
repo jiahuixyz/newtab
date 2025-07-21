1afb4876d5f7de6d0e59ce9d934eda32849ce37a
liujiahui <936775057@qq.com>
2025-07-22 01:19:55 +0800

    workbox

--- src/index.js
+++ src/index.js
@@ -9,3 +9,18 @@ import "./js/motto.js";
 //css
 import "./css/index.css"
 import "./css/note.css"
+
+// 注册 serviceWorker
+// 处理兼容性问题
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", () => {
+    navigator.serviceWorker
+      .register("/service-worker.js")
+      .then(() => {
+        console.log("sw 注册成功");
+      })
+      .catch(() => {
+        console.error("sw 注册失败");
+      });
+  });
+}
